9d11d42773b16c44df8103eec57a7f7beffc7850
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-11 15:45:36 +0200

    řešení druhé části úkolu v poznámce - minutka 3

--- index.js
+++ index.js
@@ -19,3 +19,43 @@ document.querySelector("button").addEventListener("click", (e) => {
 
     setTimeout(clearTimer, time.value * 1000)
 })
+
+
+// const nastartujMinutku = (e) => {
+//     e.preventDefault();
+//     let minutes = Number(document.querySelector('.time-input').value);
+//     let seconds = 0;
+  
+//     const minutyElm = document.querySelector('.alarm__minutes');
+//     const sekundyElm = document.querySelector('.alarm__seconds');
+//     const alarmElm = document.querySelector('.alarm');
+  
+//     // pro opakované použití
+//     alarmElm.classList.remove('alarm--ring');
+  
+//     minutyElm.textContent = String(minutes) + ':';
+//     sekundyElm.textContent = String(seconds).padStart(2, '0');
+  
+//     const odpocet = () => {
+//       if (minutes >= 0) {
+//         if (seconds > 0) {
+//           seconds -= 1;
+//         } else {
+//           minutes -= 1;
+//           seconds = 59;
+//         }
+//         minutyElm.textContent = String(minutes) + ':';
+//         sekundyElm.textContent = String(seconds).padStart(2, '0');
+//       }
+//       if (minutes <= 0 && seconds <= 0) {
+//         clearInterval(casovac);
+//         alarmElm.classList.add('alarm--ring');
+//         document.querySelector('audio').play();
+//       }
+//     };
+  
+//     const casovac = setInterval(odpocet, 1000);
+//   };
+  
+//   const formularElm = document.querySelector('.controls');
+//   formularElm.addEventListener('submit', nastartujMinutku);
