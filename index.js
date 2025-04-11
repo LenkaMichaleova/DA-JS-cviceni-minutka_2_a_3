32b47f9e26153a09ce7e59c53aed9fd354ea535b
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-11 14:53:06 +0200

    vyřešena 1. část úkolu - minutka 2

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('funguju!');
+const time = document.querySelector(".time-input")
+
+// document.querySelector(".controls").addEventListener("submit", () => {
+document.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault()    
+    document.querySelector(".alarm__seconds").textContent = time.value.padStart(2, "0")
+
+    const timerId = setInterval(() =>{
+        time.value -= 1
+        document.querySelector(".alarm__seconds").textContent = time.value.padStart(2, "0")
+    },1000)
+
+    const clearTimer = () => {
+        clearInterval(timerId)
+        new Audio("assets/alarm.mp3").play()
+    }
+
+    setTimeout(clearTimer, time.value * 1000)
+})
